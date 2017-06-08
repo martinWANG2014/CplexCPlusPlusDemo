288a782e7433dd1ef2dae71d5663f1ba8e009f7f
chenghaowang <chenghaomartin@gmail.com>
2017-06-08 17:00:03 +0200

    add comments

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CplexCPlusPlusDemo
 a simple demo of using cplex callable library for C/C++.
+# Executable Target
+## Path: /build/bin
+# Model File
+## Path: /build/model.lp
+# Solution File
+## Path: /build/sol.sl
+# Dependency Library Configuration
+## Path: /cmake/FindCplex.cmake
+# CMakeLists File
+## Path: CMakeLists.txt
--- main.cpp
+++ main.cpp
@@ -8,17 +8,20 @@
 using namespace std;
 
 int main() {
+    // cplex environment, declare only once time, release it at the end of program.
     IloEnv env;
+    // cplex model
     IloModel model(env);
+    // decision variables
     IloNumVarArray x(env);
     int size = 2;
-    //variables x0 x1
+    // variables x0 x1
     for (int i = 0; i < size; i++) {
         stringstream name;
         name << "x(" << i << ")";
         x.add(IloBoolVar(env, 0, 1, name.str().c_str()));
     }
-    //constraints
+    // constraints
     // x0 + x1 <= 1
     IloExpr constraint1(env);
     constraint1 += x[0];
@@ -31,26 +34,44 @@ int main() {
     constraint2 += -x[1];
     model.add(constraint2 >= 0);
 
-
-    //objective
+    // function objective
     // max 2*x0+3*x1
     IloExpr obj(env);
     for (int i = 0; i < size; i++) {
         obj += (i + 2) * x[i];
     }
     model.add(IloMaximize(env, obj));
-    IloCplex solver(model); // solver
-    solver.setOut(std::cout); // no output
-    solver.setParam(IloCplex::Threads, 2); // no multi-threading (less performance)
+
+    //define a solver for the constructed model.
+    IloCplex solver(model);
+
+    // set the output as stdout
+    solver.setOut(std::cout);
+
+    // use one single thread.
+    solver.setParam(IloCplex::Threads, 1);
+
+    // now , solve the model
     solver.solve();
+
+    // export the model.
     solver.exportModel("model.lp");
+
+    // export the solution.
     solver.writeSolution("sol.sl");
+
+    // get the function objective value.
     double objective_value = solver.getObjValue();
+
+    //get the decision variables values.
     IloNumArray value_variables(env);
     solver.getValues(value_variables, x);
+
+    //release all the allocated resources
     model.end();
     solver.end();
     obj.end();
+
     std::cout << "Result: " << objective_value << std::endl;
     return 0;
 }
